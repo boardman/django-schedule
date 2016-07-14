293862b0a40fe3763fdf7314bc425e5a36361376
Roger Boardman <roger@bxthree.com>
2016-07-14 13:22:13 +0100

    convert imports

--- schedule/models/__init__.py
+++ schedule/models/__init__.py
@@ -1,5 +1,5 @@
-from schedule.models.calendars import *
-from schedule.models.events import *
-from schedule.models.rules import *
+from .calendars import *
+from .events import *
+from .rules import *
 
 from schedule.signals import *
